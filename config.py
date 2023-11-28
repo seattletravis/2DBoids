6b94db8f459002391c5fbf8f9426ff27ae70bdad
Travis Lamberte <travis@lamberte.com>
2023-11-27 21:20:22 -0800

    initial commit

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 window_size = [1900, 1000]
 
 params = {
-    'max_speed' : 20,
-    'influence' : 800,
-    'min_dist' : 30,
+    'max_speed' : 3,
+    'influence' : 200,
+    'min_dist' : 10,
     'obj_margin' : 100,
     'cohesion_factor' : 0.05,
-    'separation_factor' : 0.5,
+    'separation_factor' : 1,
     'align_factor' : 0.5,
     'always_limit_speed' : True,
     'jitter' : 6,
